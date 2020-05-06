4c9dfceab4297c23a7d027866fb9be73f9485166
Ałiaksiej <61758946+Asphobel@users.noreply.github.com>
2020-05-06 15:58:45 +0300

    Add files via upload

---
+++ 2 term/TMP/3 Lab/UniProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UniProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human("Ałiaksiej", "Pieravierzieū", 18, Gender.Agender, "Biełarus", Race.White, 68, 181, "MCAllow");
+            Console.WriteLine(h1.BMICalculate());
+            Console.WriteLine(h1.ToString());
+
+        }
+    }
+}
