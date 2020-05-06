3ff2ee0d7d00582f204b9471e4dbce49225d8088
Ałiaksiej <61758946+Asphobel@users.noreply.github.com>
2020-05-06 16:08:31 +0300

    Update Lawyer.cs

--- 2 term/TMP/5 Lab/UniProject/UniProject/Lawyer.cs
+++ 2 term/TMP/5 Lab/UniProject/UniProject/Lawyer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace UniProject
 {
-    public class Lawyer : Programmer
+    public class Lawyer : Student
     {
         public Lawyer(string university, string faculty, string hometown,
                             bool isFullTime, string name, string surname, int age)
