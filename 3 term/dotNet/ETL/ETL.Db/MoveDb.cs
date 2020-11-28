936400f53d04e673913f72ab62accea6c3b50f8b
Alaksiej <aspheflaneur@gmail.com>
2020-11-28 20:05:07 +0300

    Add CreateNote() int Db

--- 3 term/dotNet/ETL/ETL.Db/MoveDb.cs
+++ 3 term/dotNet/ETL/ETL.Db/MoveDb.cs
@@ -16,5 +16,11 @@ namespace ETL.Db
             var id = _context.Notes.DefaultIfEmpty().Max(note => note == null ? 0 : note.Id);
             return id + 1;
         }
+        public void CreateNote(int id, string creationDate, string content)
+        {
+            var note = new Note {Id = id, CreationDate = creationDate, Content = content};
+            _context.Notes.Add(note);
+            _context.SaveChanges();
+        }
     }
 }
